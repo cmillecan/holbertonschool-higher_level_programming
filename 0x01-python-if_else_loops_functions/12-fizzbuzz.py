b878dce6d0a55ad3660cb60e8e360589e70d0491
cmillecan <cmillecan@gmail.com>
2020-04-29 00:41:38 +0000

    add file

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 100):
+        if num % 3 == 0 and num % 5 == 0:
+            print("Fizzbuzz", end=' ')
+            continue
+        elif num % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        elif num % 5 == 0:
+            print("Buzz", end=' ')
+            continue
+        else:
+            print("{:d}".format(num), end=' ')
