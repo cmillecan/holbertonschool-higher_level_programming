32671918f4fec1010b1abed2975179077d534aae
cmillecan <cmillecan@gmail.com>
2020-04-29 02:40:41 +0000

    add file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,9 +14,9 @@ listint_t *insert_node(listint_t **head, int number)
 	if (head == NULL)
 		return (NULL);
 
-	new_node->data = number;
+	new_node->n = number;
 	new_node->next = head;
-	head = new_node;
+	*head = new_node;
 
 	return (head);
 }
