a51f27f0e4d2ebeed1d81e90b89f591155d7092c
cmillecan <cmillecan@gmail.com>
2020-05-01 06:11:07 +0000

    add file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(argv)))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
