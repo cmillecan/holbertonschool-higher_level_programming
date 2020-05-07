eff78395c5b83b5d02d02443b3a15fd7ac6e0ccc
cmillecan <cmillecan@gmail.com>
2020-05-07 21:26:39 +0000

    add file

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set1 = set(set_1)
-    intersect = set1.intersection(set_2)
-    intersect_list = list(intersect)
-    return intersect_list
+    return list(set(set_1) & set(set_2))
