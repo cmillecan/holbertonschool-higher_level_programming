25c83766bd199eee05646925c53dd3a7207a5052
cmillecan <cmillecan@gmail.com>
2020-05-20 06:25:40 +0000

    add file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,8 +3,8 @@
 
 
 class Square:
+    """Defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes data"""
         self.size = size
         self.position = position
 
