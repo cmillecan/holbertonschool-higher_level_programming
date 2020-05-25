a4a0e13fee0afdd77afb405afb72e11698bef2ac
cmillecan <cmillecan@gmail.com>
2020-05-25 22:53:35 +0000

    add file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,16 +4,17 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Test for max_integer"""
 
     def text_max(self):
         """Tests for valid integers"""
-        self.assertEqual(max_integer([2, 4, 9, 1]), 9)
+        self.assertEqual(max_integer([2, 4, 1, 9]), 9)
         self.assertEqual(max_integer([9, 1, 4, 2]), 9)
-        self.assertEqual(max_integer([-2, -4, -9, -1]), -1)
+        self.assertEqual(max_integer([-2, -4, -1, -9]), -1)
         self.assertEqual(max_integer([25]), 25)
-        self.assertEqual(max_integer([2, 4, -9, -1]), 4)
+        self.assertEqual(max_integer([2, 4, -9, 1]), 4)
         self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
