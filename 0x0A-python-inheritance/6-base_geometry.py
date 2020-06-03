f46e9c776a9f5249b1f54471cd87aa37870e0373
cmillecan <cmillecan@gmail.com>
2020-06-03 04:04:27 +0000

    add file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Raises exception
+        """
+        raise Exception('area() is not implemented')
