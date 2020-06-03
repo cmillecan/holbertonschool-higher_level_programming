ce3dad796ca5207f71685f8247c7ef856c733d02
cmillecan <cmillecan@gmail.com>
2020-06-03 04:52:14 +0000

    add file

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,11 +9,10 @@ class MyInt(int):
     Inherits from int
     """
 
-    def MyInt():
-        def __eq__(self, other):
-            """Equal to"""
-            return int(self) != int(other)
+    def __eq__(self, other):
+        """Equal to"""
+        return int(self) != int(other)
 
-        def __ne__(self, other):
-            """Not equal to"""
-            return int(self) == int(other)
+    def __ne__(self, other):
+        """Not equal to"""
+        return int(self) == int(other)
