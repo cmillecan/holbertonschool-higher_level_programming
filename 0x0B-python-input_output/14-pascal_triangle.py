2e98b2f284bfaffcffa2df11a29c7e7c9f20f4da
cmillecan <cmillecan@gmail.com>
2020-06-06 00:21:28 +0000

    add file

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Task 14 """
 
+
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the 
+    Returns a list of lists of integers representing the
     Pascal’s triangle of n.
     """
     row = [1]
