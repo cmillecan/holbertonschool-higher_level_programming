7f087a775db3fbfffd678cc7e4018346619a0ffc
cmillecan <cmillecan@gmail.com>
2020-06-10 06:10:05 +0000

    tests file

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Unittest for Rectangle
+"""
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+    """Test Rectangle"""
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest for Square
+"""
+
+import unittest
+from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestSquare(unittest.TestCase):
+    """Class TestSquare"""
