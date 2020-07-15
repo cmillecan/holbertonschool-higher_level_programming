477bf56c32755a330512ce377c31a5563e3fe490
cmillecan <cmillecan@gmail.com>
2020-07-15 03:31:17 +0000

    add file

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- Cities of California
+-- List all the cities of California that can be found in a database
+SELECT `id`, `name` FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
