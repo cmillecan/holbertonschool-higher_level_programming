9328870552751c859b7c12a208f0c951cca2bdb5
cmillecan <cmillecan@gmail.com>
2020-08-10 02:31:29 +0000

    add file

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name
-                 FROM cities JOIN states ON cities.state_id = states.id
-                 WHERE states.name=%s ORDER BY cities.id""", (argv[4], ))
+    cur.execute("""SELECT cities.name FROM cities
+                 JOIN states ON cities.state_id = states.id
+                 WHERE states.name = %s ORDER BY cities.id""", (argv[4], ))
 
     rows = cur.fetchall()
     for row in rows:
