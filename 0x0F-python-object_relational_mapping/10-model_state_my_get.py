b0b65bb63d1ff32896ccd8c52378c1799beabc1d
cmillecan <cmillecan@gmail.com>
2020-08-10 05:25:31 +0000

    add file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.name == argv[4]).\
         order_by(State.id).all()
     if state:
-        print(state.id)
+        print("{}".format(state.id))
     else:
         print("Not found")
 
