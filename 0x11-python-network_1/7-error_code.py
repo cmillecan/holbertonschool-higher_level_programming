f903ce99f083b7634f938a5aeb1d54f7453ab40e
cmillecan <cmillecan@gmail.com>
2020-08-25 04:23:53 +0000

    add file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,3 +3,12 @@
 Write a Python script that takes in a URL, sends a request
 to the URL and displays the body of the response.
 """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code < 400:
+        print(req.text)
+    else:
+        print('Error code: {}'.format(req.status_code))
