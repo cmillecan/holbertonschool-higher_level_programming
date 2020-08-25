d7ea07c484d75582f1bb9d270d3e41525da16477
cmillecan <cmillecan@gmail.com>
2020-08-25 03:25:22 +0000

    add file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
     try:
         with urllib.request.urlopen(arg) as response:
             print(response.read().decode('utf-8'))
-    except error.HTTPError as error:
-        print('Error code:', error.code)
+    except urllib.error.HTTPError as error:
+        print('Error code: {}'.format(error.code))
