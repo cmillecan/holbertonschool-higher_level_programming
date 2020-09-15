7e2c2e45f3051556223231550144c26caa0e3504
cmillecan <cmillecan@gmail.com>
2020-09-15 20:24:03 +0000

    add file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
+  const array = process.argv.slice(2);
   array.sort();
   console.log(array[array.length - 2]);
 }
