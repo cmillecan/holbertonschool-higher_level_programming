c5c4fdef11cb7dc026fc3ce7ea2fcabe77ce6843
cmillecan <cmillecan@gmail.com>
2020-09-22 00:37:14 +0000

    add file

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let left = 0;
+  let right = list.length - 1;
+  while (left < right) {
+    const temp = list[left];
+    list[left] = list[right];
+    list[right] = temp;
+    left++;
+    right--;
+  }
+  return list;
+};
