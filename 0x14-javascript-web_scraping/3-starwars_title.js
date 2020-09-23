d31d7bdc511994ef9a2e60461a953db6ab41fbd8
cmillecan <cmillecan@gmail.com>
2020-09-23 05:50:17 +0000

    add file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  const info = JSON.parse(body);
+  console.log(info.title);
+});
